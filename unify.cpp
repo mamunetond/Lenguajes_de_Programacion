abe7c8795e5de3e1c03fd2b3e48aa1384dfc1b91
mamunetond <47578828+mamunetond@users.noreply.github.com>
2019-11-12 21:09:46 -0500

    Version 0.1
    Lectura de archivos

--- unify.cpp
+++ unify.cpp
@@ -1,8 +1,36 @@
+#include <fstream>
+#include <string.h>
+#include <stdlib.h>
 #include <iostream>
-#include <string>
-
 using namespace std;
 
-int main():
-  return 0;
+void lectura();
 
+int main()
+{
+  lectura();
+  system("pause");
+  return 0;
+}
+
+void lectura()
+{
+  ifstream archivo;
+  string texto;
+  archivo.open("Pruebas.txt",ios::in); // Abrimos el archivo en modo lectura
+
+  if(archivo.fail())
+    {
+      cout << "No se pudo abrir el archivo";
+      exit(1);
+    }
+
+    while(!archivo.eof())  // Mientras no sea final de fichero
+      {
+	getline(archivo,texto);
+	cout << texto << endl;
+      }
+
+    archivo.close(); // Cerramos el archivo
+    
+}
