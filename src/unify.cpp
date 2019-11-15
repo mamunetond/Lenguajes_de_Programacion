a3af801ece2e65ca43017e5bcdf3589eb05cb8af
mamunetond <47578828+mamunetond@users.noreply.github.com>
2019-11-15 06:14:05 -0500

    unify.cpp

--- src/unify.cpp
+++ src/unify.cpp
@@ -2,9 +2,13 @@
 #include <string.h>
 #include <stdlib.h>
 #include <iostream>
+
+
 using namespace std;
 
+
 void lectura();
+
 
 int main()
 {
@@ -15,11 +19,13 @@ int main()
 
 void lectura()
 {
+
   ifstream archivo;
   string texto;
-  archivo.open("Pruebas.txt",ios::in); // Abrimos el archivo en modo lectura
-
-  if(archivo.fail())
+  int acum = 0; 
+  int aux = 0;
+  archivo.open("prueba1.txt",ios::in); // Abrimos el archivo en modo lectura
+   if(archivo.fail())
     {
       cout << "No se pudo abrir el archivo";
       exit(1);
@@ -27,10 +33,16 @@ void lectura()
 
     while(!archivo.eof())  // Mientras no sea final de fichero
       {
-	getline(archivo,texto);
-	cout << texto << endl;
+      	acum++;
+        getline(archivo,texto);
+        string numeros[acum];
+        numeros[aux] = texto;
+        cout << numeros[aux] << endl;
+        aux++;
+        
       }
-
+          
     archivo.close(); // Cerramos el archivo
-    
+
 }
+
