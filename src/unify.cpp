2f725d67c2db49e3c58e48eefe3b6f7aa761fcfc
mamunetond <47578828+mamunetond@users.noreply.github.com>
2019-11-15 19:08:06 -0500

    unify.cpp

--- src/unify.cpp
+++ src/unify.cpp
@@ -8,6 +8,12 @@ using namespace std;
 
 
 void lectura();
+void vacio();
+void fallo();
+void unificar(string C[100]);
+void VariablesLibres();
+void composicion();
+void sustitucion();
 
 
 int main()
@@ -24,7 +30,7 @@ void lectura()
   string texto;
   int acum = 0; 
   int aux = 0;
-  archivo.open("prueba1.txt",ios::in); // Abrimos el archivo en modo lectura
+  archivo.open("prueba6.txt",ios::in); // Abrimos el archivo en modo lectura
    if(archivo.fail())
     {
       cout << "No se pudo abrir el archivo";
@@ -38,11 +44,40 @@ void lectura()
         string numeros[acum];
         numeros[aux] = texto;
         cout << numeros[aux] << endl;
+        unificar(numeros);
         aux++;
         
       }
           
     archivo.close(); // Cerramos el archivo
 
+
 }
+
+void vacio()
+{
+	cout << "El archivo se encuentra vacio" << endl;
+}
+
+void falla()
+{
+	cout << "No se puede unificar" << endl;
+}
+
+void unififcar(string C[100])
+{
+		if(C -> empty())
+		{
+		   vacio();
+		   
+		}
+}
+
+
+
+
+
+
+
+
 
