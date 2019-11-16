46a527b3337f18828047ccdd730f1b6e57e3dad3
mamunetond <47578828+mamunetond@users.noreply.github.com>
2019-11-15 20:36:07 -0500

    ast.cpp

--- src/ast.cpp
+++ src/ast.cpp
@@ -35,9 +35,6 @@ BinaryNode::~BinaryNode() {
 AST* BinaryNode::getLeftSubTree() const {
    return leftTree;
 }
-
-AST* BinaryNode::getRightSubTree() const {
-   return rightTree;
 }
 
 UnaryNode::UnaryNode(AST* sub):
@@ -59,53 +56,16 @@ AST* UnaryNode::getSubTree() const {
    return subTree;
 }
 
-AddNode::AddNode(AST* left, AST* right):
-   BinaryNode(left,right)
-{}
-
-int AddNode::evaluate() {
-   return getLeftSubTree()->evaluate() + getRightSubTree()->evaluate();
-}
-
-s
-
 IdNode::IdNode(std::string var) :
    AST(),
    var(var)
 {}
 
-int IdNode::evaluate() {
-  if(calc->containVar(var))
-    return calc->getVar(var);
-  else
-    calc->setVar(var, 0);
-    return 0;
+
 }
-
-string IdNode::compile() {
-
-  string line = "";
-  evaluate();
-  return line + "\n# VarNode \n"+
-         "M[SP+0] := " + var + "\n"+
-         "SP := SP - UNO \n";
-}
-
-
-RecallNode::RecallNode() :
-   AST()
-{}
 
 int RecallNode::evaluate() {
   return calc->recall();
-}
-
- string RecallNode::compile() {
-
-  string line = "";
-  return line + "\n# RecallNode \n"+         
-         "SP      := SP + UNO \n"+
-         "M[SP+1] := MEMORY \n";
 }
 
 InitVarNode::InitVarNode(std::string var, AST* sub) : UnaryNode(sub), var(var) {}
@@ -123,9 +83,7 @@ StoreNode::StoreNode(AST *sub) : UnaryNode(sub) { }
 StoreNode::~StoreNode() {}
 
 int StoreNode::evaluate() {
-  int result = getSubTree()->evaluate();
-  calc->store(result);
-    
+  int result = getSubTree()->evaluate();   
 }
 }
 
@@ -136,13 +94,6 @@ ClearNode::ClearNode() :
 ClearNode::~ClearNode() {}
 
 int ClearNode::evaluate() {
-  return calc->clear();
+  
 }
-PlusNode::PlusNode(AST *sub) : UnaryNode(sub) { }
 
-PlusNode::~PlusNode() {}
-
-int PlusNode::evaluate() {
-  calc->plus(getSubTree()->evaluate());
-  return calc->recall();  
-}
